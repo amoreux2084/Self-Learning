69026b02f34948aedfce10a17e53b798adfd6b04
Amoreux2084 <taisiya.karipidis@gmail.com>
2023-09-19 21:20:24 +0000

    qqcoisa

--- Programming/Programming_Fundamentals/task1.py
+++ Programming/Programming_Fundamentals/task1.py
@@ -2,10 +2,11 @@ line001 = '*************************'
 line002 = '*************************'
 line003 = '*************************'
 line004 = '*        welcome        *'
-        line005 = '***'
+line005 = '***'
 # now print
 print('')
 print(line001)
 print(line002)
 print(line003)
 print(line004)
+        print(line001)
